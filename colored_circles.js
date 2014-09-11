3aa012b0dd021aacc8251ab8e73483e18d7218f6
Evan Simoni <evan.simoni@gmail.com>
2014-09-10 21:08:46 -0700

    done working on this for the night. to be continued

--- colored_circles.js
+++ colored_circles.js
@@ -2,7 +2,7 @@
 
 // div class stars?
 
-
+// This doesn't work yet btw..............
 var scores = $('.circle h3')
 
 var setColor = function() {
@@ -11,32 +11,32 @@ var setColor = function() {
     innerScore = $(particularScoreDiv).html()
     
     switch (true) {
-      case (innerScore >= 0 && innerScore <= 30):
-        $('.circle').css({'background-color': '#F26C4F'}) // RED
-        break;
-      case (innerScore >= 30 && innerScore < 40):
-        $('.circle').css({'background-color': '#F68E55'})
-        break;
-      case (innerScore >= 40 && innerScore < 50):
-        $('.circle').css({'background-color': '#FBAF5C'})
-        break;
-      case (innerScore >= 50 && innerScore < 60):
-        $('.circle').css({'background-color': '#FFF467'})
-        break;
-      case (innerScore >= 60 && innerScore < 70):
-        $('.circle').css({'background-color': '#ACD372'})
+      case (innerScore >= 80 && innerScore <= 100):
+        $('.circle').css({'background-color': '#3BB878'})  // GREEN
         break;
       case (innerScore >= 70 && innerScore < 80):
         $('.circle').css({'background-color': '#7CC576'})
         break;
-      case (innerScore >= 80 && innerScore <= 100):
-        $('.circle').css({'background-color': '#3BB878'})  // GREEN
+      case (innerScore >= 60 && innerScore < 70):
+        $('.circle').css({'background-color': '#ACD372'})
+        break;
+      case (innerScore >= 50 && innerScore < 60):
+        $('.circle').css({'background-color': '#FFF467'})
+        break;
+      case (innerScore >= 40 && innerScore < 50):
+        $('.circle').css({'background-color': '#FBAF5C'})
+        break;
+      case (innerScore >= 30 && innerScore < 40):
+        $('.circle').css({'background-color': '#F68E55'})
+        break;
+      case (innerScore >= 0 && innerScore <= 30):
+        $('.circle').css({'background-color': '#F26C4F'}) // RED
         break;
     }   
   }
 }
 
-
+// Index needs to be modified for this to work ^^
 <div class="circle">
   <div class="stars"></div>
     <h3><%=Math.round(average_score*20)%></h3>
