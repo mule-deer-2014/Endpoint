cebd601763592a9e91318af620f3ff7d5f062838
Evan Simoni <evan.simoni@gmail.com>
2014-09-10 21:00:38 -0700

    sets up js case statement to check color

--- colored_circles.js
+++ colored_circles.js
@@ -3,19 +3,36 @@
 // div class stars?
 
 
-// color = $('.circle').css({'background-color': '#99cc33'})
 var scores = $('.circle h3')
-function setColor(scores) {
-  for (i = 0 ; i < scores.length; i++) { 
-    score = $('.circle h3').html() 
-    if (parseInt(score) > 50) {
-      console.log(parseInt(score))
-      $('.circle').css({'background-color': '#008000'})
-    }
-    else {
-      console.log(parseInt(score))
-      $('.circle').css({'background-color': '#FF0000'})
-    }
+
+var setColor = function() {
+  for (i = 0 ; i < scores.length; i++) {
+    particularScoreDiv = scores[i]
+    innerScore = $(particularScoreDiv).html()
+    
+    switch (true) {
+      case (innerScore >= 0 && innerScore <= 30):
+        $('.circle').css({'background-color': '#F26C4F'}) // RED
+        break;
+      case (innerScore >= 30 && innerScore < 40):
+        $('.circle').css({'background-color': '#F68E55'})
+        break;
+      case (innerScore >= 40 && innerScore < 50):
+        $('.circle').css({'background-color': '#FBAF5C'})
+        break;
+      case (innerScore >= 50 && innerScore < 60):
+        $('.circle').css({'background-color': '#FFF467'})
+        break;
+      case (innerScore >= 60 && innerScore < 70):
+        $('.circle').css({'background-color': '#ACD372'})
+        break;
+      case (innerScore >= 70 && innerScore < 80):
+        $('.circle').css({'background-color': '#7CC576'})
+        break;
+      case (innerScore >= 80 && innerScore <= 100):
+        $('.circle').css({'background-color': '#3BB878'})  // GREEN
+        break;
+    }   
   }
 }
 
